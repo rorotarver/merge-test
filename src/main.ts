35ddc7e27862c79da7176bad666f52ea68a120d5
rorotarver <roro9722@outlook.com>
2025-11-04 14:42:37 +0100

    added interface Task, added tasks array, created a list called 'todo'

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 import './style.css'
 
-const addTodoBtn = document.getElementById('addTodobtn');
+interface Task {
+    text: string;
+    completed: boolean;
+    archived?: boolean;
+}
+    
+
+let tasks: Task [] = [];
+
+const storedTasks = localStorage.getItem('tasks');
+
+if (storedTasks) {
+    tasks = JSON.parse(storedTasks) as Task[];
+}
+
+
+const addTodoText = document.getElementById('skrivTxt') as HTMLInputElement;
+
+const addTodoBtn = document.getElementById('addTodobtn') as HTMLButtonElement;
+
+const todo = document.getElementById('todo') as HTMLUListElement;
