09302e44bb6247c6b9e12c005d47c7735fe2acbd
Ibbson <ibbe30@gmail.com>
2025-11-04 15:04:20 +0100

    Added functions to add items to the Todolist

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,46 @@ const addTodoText = document.getElementById('skrivTxt') as HTMLInputElement;
 const addTodoBtn = document.getElementById('addTodobtn') as HTMLButtonElement;
 
 const todo = document.getElementById('todo') as HTMLUListElement;
+
+function addTask() {
+    const text = addTodoText.value.trim(); 
+
+    if (text === "") return; 
+
+    const newTask: Task = {
+        text: text,
+        completed: false
+    };
+
+    tasks.push(newTask); 
+    localStorage.setItem('tasks', JSON.stringify(tasks)); 
+    renderTasks();
+    addTodoText.value = ""; 
+}
+
+addTodoBtn.addEventListener('click', addTask);
+
+function renderTasks() {
+    todo.innerHTML = ""; 
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.textContent = task.text;
+        if (task.completed) {
+            li.style.textDecoration = 'line-through';
+        }
+
+        
+        const doneBtn = document.createElement('button');
+        doneBtn.textContent = '✔';
+        doneBtn.addEventListener('click', () => {
+            tasks[index].completed = !tasks[index].completed;
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            renderTasks();
+        });
+
+        li.appendChild(doneBtn);
+        todo.appendChild(li);
+    });
+}
+
